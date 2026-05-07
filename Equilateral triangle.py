8d485ef92f70e85672866252884bdeae0b0c2883
ayeshamanzoor914 <ayeshamanzoor914@gmail.com>
2026-05-06 23:01:54 -0700

    Add files via upload

---
+++ Equilateral triangle.py
@@ -0,0 +1,12 @@
+# for i in range(1,11): # to print tables
+#     for j in range(1,11):
+#         print(i*j,end="\t")
+#     print()
+
+# for i in range(1,5): # to print equilateral triangle
+#     for j in range(4-i):
+#         print(" ",  end=" ")
+#     for j in range(2*i-1):
+#         print("*", end=" ")
+#     print()
+
